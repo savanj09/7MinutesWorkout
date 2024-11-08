f250e79704d9bd84d486d4597dee1f9a33d43881
savanj09 <jsavan09@gmail.com>
2024-11-08 21:53:24 +0530

    Added Workout History.

--- app/src/main/java/com/minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/minutesworkout/FinishActivity.kt
@@ -1,11 +1,17 @@
 package com.minutesworkout
 
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class FinishActivity : AppCompatActivity() {
     private var binding: ActivityFinishBinding?=null
@@ -28,9 +34,29 @@ class FinishActivity : AppCompatActivity() {
             finish()
         }
 
+        val dao = (application as WorkOutApp).db.historyDao()
+
+        addDatetoDatabase(dao)
+
     }
 
-    private fun addDatetoDatabase(){
+    private fun addDatetoDatabase(historyDao: HistoryDao){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date",""+dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss",Locale.getDefault())
+        val date = sdf.format(dateTime)
+
+        Log.e("Formated Date : ",""+date)
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+
+            Log.e("Date : ","Added...")
+
+
+        }
 
 
     }
--- app/src/main/java/com/minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/minutesworkout/HistoryActivity.kt
@@ -1,11 +1,17 @@
 package com.minutesworkout
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding:ActivityHistoryBinding?= null
@@ -25,5 +31,50 @@ class HistoryActivity : AppCompatActivity() {
 
         }
 
+        val dao = (application as WorkOutApp).db.historyDao()
+        getAllCompletedDates(dao)
+
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao){
+
+        lifecycleScope.launch {
+
+            historyDao.fetchAllDates().collect{ allCompletedDatesList ->
+
+                if (allCompletedDatesList.isNotEmpty()){
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility =View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.GONE
+
+                    val dates = ArrayList<String>()
+                    for (date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+                    val historyAdapter = HistoryAdapter(ArrayList(dates))
+                    binding?.rvHistory?.adapter = historyAdapter
+
+                }
+                else{
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility =View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+                }
+                for (i in allCompletedDatesList){
+
+                    Log.e("Date : ", ""+i.date)
+                }
+
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
     }
 }
